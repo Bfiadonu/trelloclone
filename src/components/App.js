1ea94f1329a35f2fbff3f23b3bf5dc55c375c240
bfiadonu <bfiadonu@gmail.com>
2020-08-28 10:51:07 -0400

    trellocomm

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import TrelloList from './TrelloList';
+import { connect } from "react-redux";
+import TrelloActionButton from "./TrelloActionButton";
+import { DragDropContext } from "react-beautiful-dnd";
+
+class App extends Component {
+  
+  onDragEnd = () => {
+    
+  };
+  
+  render() {
+    
+    const { lists } = this.props;
+  return (
+    <DragDropContext onDragEnd={this.onDragEnd}>
+    <div>
+      <h2>Hello Youtube</h2>
+    <div style={styles.listsContainer}>
+    {lists.map(list => (
+    <TrelloList 
+    listID={list.id}
+    key={list.id} 
+    title={list.title} 
+    cards={list.cards} />
+  ))}
+  <TrelloActionButton list />
+    </div>
+  </div>
+</DragDropContext>
+  );
+}
+}
+
+const styles = {
+ listsContainer: {
+   display: "flex",
+   flexDirection: "row",
+   marginRight: 8
+ } 
+};
+
+const mapStateToProps = state => ({
+  lists: state.lists
+});
+
+export default connect(mapStateToProps)(App);
